be427b7dda1321cc5f83f79a87b63736027d6b61
smj <newgit@samj.app>
2025-05-24 21:27:54 +0100

    aa

--- src/providers/sources/oneroom.ts
+++ src/providers/sources/oneroom.ts
@@ -62,7 +62,7 @@ async function comboScraper(ctx: ShowScrapeContext | MovieScrapeContext): Promis
 export const oneroomScraper = makeSourcerer({
   id: 'oneroom',
   name: 'OneRoom 🦭',
-  rank: 179,
+  rank: 178,
   disabled: false,
   flags: [flags.CORS_ALLOWED],
   scrapeMovie: comboScraper,
